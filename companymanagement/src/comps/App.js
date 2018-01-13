631a72592fd6b34ddf9d130d48e552993176244a
prathyusha <prathyusha.system@gmail.com>
2018-01-13 15:15:49 -0500

    Company Management Application Console

---
+++ companymanagement/src/comps/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import CompanyManagement from './CompanyManagement';
+import '../App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+
+      <CompanyManagement/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
